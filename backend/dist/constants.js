c4fff5cd379b2ab56dad8a9980495e46c6dc8ec9
Ana <anayfernandez95@gmail.com>
2024-01-04 22:08:49 -0500

    backend auth

---
+++ backend/dist/constants.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Roles = void 0;
+var Roles;
+(function (Roles) {
+    Roles["Admin"] = "ADMIN";
+    Roles["Lead"] = "LEAD";
+    Roles["ProjectManager"] = "PROJECT_MANAGER";
+    Roles["User"] = "USER";
+})(Roles || (exports.Roles = Roles = {}));
---
+++ backend/src/middleware/authMiddleware.ts
@@ -0,0 +1,59 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload, TokenExpiredError } from "jsonwebtoken";
+import User from "../models/user";
+import Token from "../models/token";
+import asyncHandler from "express-async-handler";
+import { AuthenticationError } from "./errorMiddleware";
+
+const authenticate = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let token = req.cookies.jwt;
+
+      if (!token) {
+        throw new AuthenticationError("Token not found");
+      }
+      
+      let validationToken = await Token.findOne({jwt: token});
+
+      if (!validationToken) {
+        await Token.create({jwt: token, attemps: 0, locked: false});
+        validationToken = await Token.findOne({jwt: token});
+      }
+
+      const addAttemp = () => {
+        validationToken!.attempts++;
+        if (validationToken!.attempts >= 3) {
+          validationToken!.locked = true;
+        }
+        Token.updateOne({jwt: validationToken!.jwt}, {...validationToken})
+      }
+      
+      if (validationToken!.locked) {
+        throw new AuthenticationError("JWT locked");
+      }
+
+      const jwtSecret = process.env.JWT_SECRET || "";
+      const decoded = jwt.verify(token, jwtSecret) as JwtPayload;
+
+      if (!decoded || !decoded.userId) {
+        addAttemp();
+        throw new AuthenticationError("UserId not found");
+      }
+
+      const user = await User.findById(decoded.userId, "_id name email");
+
+      if (!user) {
+        addAttemp();
+        throw new AuthenticationError("User not found");
+      }
+
+      req.user = user;
+      next();
+    } catch (e) {
+      throw new AuthenticationError("Invalid token");
+    }
+  }
+);
+
+export { authenticate };
